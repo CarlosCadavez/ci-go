2610d2062142cfdb8959ce088055ad9068193da0
CarlosCadavez <cs.cadavez@gmail.com>
2021-01-31 01:19:53 -0300

    test: fix test assert condit

--- math_test.go
+++ math_test.go
@@ -7,7 +7,7 @@ import (
 func TestSoma(t *testing.T) {
 	total := soma(15, 15)
 
-	if total != 31 {
+	if total != 30 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 30)
 	}
 }
